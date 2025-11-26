7f3dbea69acd203aca8721ce9fbb9f1656c05211
Jonathan Manik <jonathanmanikraja@gmail.com>
2025-11-27 05:37:10 +0700

    update

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -3,20 +3,16 @@
 namespace App\Services;
 
 use Kreait\Firebase\Factory;
-use Kreait\Firebase\Database;
 use Kreait\Firebase\Messaging;
 
 class FirebaseService
 {
-    protected Database $database;
     protected Messaging $messaging;
 
     public function __construct()
     {
-        // Path sementara untuk JSON di storage
         $credentialsPath = storage_path('app/firebase/firebase-key.json');
 
-        // Jika file belum ada, buat dari env
         if (!file_exists($credentialsPath)) {
             if (!env('FIREBASE_JSON')) {
                 throw new \Exception("Firebase JSON not set in environment variables.");
@@ -25,16 +21,9 @@ class FirebaseService
         }
 
         $factory = (new Factory)
-            ->withServiceAccount($credentialsPath)
-            ->withDatabaseUri(env('FIREBASE_DATABASE_URL', 'https://laravelfirebasedemo-417d7-default-rtdb.firebaseio.com'));
+            ->withServiceAccount($credentialsPath);
 
-        $this->database = $factory->createDatabase();
         $this->messaging = $factory->createMessaging();
-    }
-
-    public function getDatabase(): Database
-    {
-        return $this->database;
     }
 
     public function getMessaging(): Messaging
